4e30f9827708972c3c5d531baa25bdb0f8b851b4
Dmytro KUCHURA <dmytro.kuchura@raiffeisen.ua>
2022-01-05 09:15:42 +0200

    update swagger

--- src/app/Http/Controllers/Api/EpisodeController.php
+++ src/app/Http/Controllers/Api/EpisodeController.php
@@ -47,6 +47,40 @@ class EpisodeController extends Controller
         ]);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/v1/episodes/create",
+     *     summary="Create new episode",
+     *     tags={"Episodes"},
+     *     @OA\RequestBody(
+     *        required=true,
+     *        description="Create new episode",
+     *        @OA\JsonContent(
+     *           required={"title","format","timezone","language","explicit","category"},
+     *           @OA\Property(property="title", type="string", example="Exapmle show name"),
+     *           @OA\Property(property="description", type="string", example="Exapmle show description"),
+     *           @OA\Property(property="artwork", type="string", example=""),
+     *           @OA\Property(property="format", type="string", example="episodic"),
+     *           @OA\Property(property="timezone", type="string", example="Etc/GMT"),
+     *           @OA\Property(property="language", type="string", example="en"),
+     *           @OA\Property(property="explicit", type="boolean", example="true"),
+     *           @OA\Property(property="category", type="string", example="sport"),
+     *           @OA\Property(property="author", type="string", example="Author name"),
+     *           @OA\Property(property="podcast_owner", type="string", example="example author"),
+     *           @OA\Property(property="email_owner", type="string", example="example@domain.com"),
+     *           @OA\Property(property="copyright", type="string", example="copyright"),
+     *        ),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation"
+     *     ),
+     *     @OA\Response(
+     *         response="401",
+     *         description="Unauthorized user"
+     *     )
+     * )
+     */
     public function create(CreateEpisodeRequest $request): JsonResponse
     {
         $this->service->createEpisode($request->all());
