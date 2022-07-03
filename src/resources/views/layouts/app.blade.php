64ef563b55543c7e20ed1ae6033e725b3938df92
Dmytro Kuchura <kuchura.d@gmail.com>
2022-07-03 22:30:44 +0300

    rewrite js

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -10,16 +10,7 @@
     <link rel="shorBtcut icon" type="image/x-icon" href="img/favicon.ico">
 
     <!-- CSS here -->
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/animate.min.css">
-    <link rel="stylesheet" href="/css/magnific-popup.css">
-    <link rel="stylesheet" href="/css/fontawesome-all.min.css">
-    <link rel="stylesheet" href="/css/dripicons.css">
-    <link rel="stylesheet" href="/css/slick.css">
-    <link rel="stylesheet" href="/css/default.css">
-    <link rel="stylesheet" href="/css/swiper.css">
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/responsive.css">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
 
     <!-- csrf-token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
